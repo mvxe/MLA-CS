9b671764e8e835b8ae558795fb6be4deec72d468
Mario Vretenar <mario.vretenar@utwente.nl>
2018-08-06 11:11:02 +0200

    -added some settings gui

--- main.cpp
+++ main.cpp
@@ -9,31 +9,31 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    std::string ret;
-    ComM_XPS xyz_ctrl;
-    std::cout << "connecting...\n";
-    try { xyz_ctrl.connect("192.168.0.254","5001"); }
-    catch (int &a) {std::cout << "error:" << a << "\n";}
-    std::cout << "done.\n";
+//    std::string ret;
+//    ComM_XPS xyz_ctrl;
+//    std::cout << "connecting...\n";
+//    try { xyz_ctrl.connect("192.168.0.254","5001"); }
+//    catch (int &a) {std::cout << "error:" << a << "\n";}
+//    std::cout << "done.\n";
 
 
-    try{ xyz_ctrl.write("GroupInitialize (GROUP1)"); }
-    catch (boost::system::system_error err) { std::cout << err.what();}
-    xyz_ctrl.read(ret);    std::cout << ret;
-    std::cout << "\n";
+//    try{ xyz_ctrl.write("GroupInitialize (GROUP1)"); }
+//    catch (boost::system::system_error err) { std::cout << err.what();}
+//    xyz_ctrl.read(ret);    std::cout << ret;
+//    std::cout << "\n";
 
-    xyz_ctrl.write("GroupHomeSearchAndRelativeMove (GROUP1,0)");
-    xyz_ctrl.read(ret);    std::cout << ret;
-    std::cout << "\n";
+//    xyz_ctrl.write("GroupHomeSearchAndRelativeMove (GROUP1,0)");
+//    xyz_ctrl.read(ret);    std::cout << ret;
+//    std::cout << "\n";
 
-    xyz_ctrl.write("GroupKill (GROUP1)");
-    xyz_ctrl.read(ret);    std::cout << ret;
-    std::cout << "\n";
+//    xyz_ctrl.write("GroupKill (GROUP1)");
+//    xyz_ctrl.read(ret);    std::cout << ret;
+//    std::cout << "\n";
 
-    xyz_ctrl.rw("FirmwareVersionGet (char *)",ret);
-    std::cout << ret;
+//    xyz_ctrl.rw("FirmwareVersionGet (char *)",ret);
+//    std::cout << ret;
 
-    std::cout << "\n";
+//    std::cout << "\n";
 
     return a.exec();
 }
