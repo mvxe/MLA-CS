2151499010fecb12755ca5b527b91c20036f4e63
Mario Vretenar <m.vretenar@utwente.nl>
2021-10-06 19:29:33 +0200

    fixed report bug

--- PROCEDURES/UTIL/USC/position_report.cpp
+++ PROCEDURES/UTIL/USC/position_report.cpp
@@ -20,8 +20,8 @@ void pgPosRepGUI::update(){
             go.pRPTY->mux.unlock();
             if(pos[0]!=old[0] || pos[1]!=old[1] || pos[2]!=old[2]){
                 for(int i=0;i!=3;i++) old[i]=pos[i];
-                this->setText(QString::fromStdString(util::toString("Stages (raw):\n  X=", std::fixed, std::setprecision(6), pos[0], "mm , Y=", pos[1], "mm\n  Z=", pos[2], "mm\n",
-                    "Corrected:\n  X=", std::fixed, std::setprecision(6), pos[1], "mm , Y=", pos[2], "mm\n  Z=", pos[3], "mm"
+                this->setText(QString::fromStdString(util::toString("Stages (raw):\n  X=", std::fixed, std::setprecision(6), pos[0], "mm , Y=", pos[1], "mm , Z=", pos[2], "mm\n",
+                    "Corrected:\n  X=", std::fixed, std::setprecision(6), pos[3], "mm , Y=", pos[4], "mm , Z=", pos[5], "mm"
                 )));
                 Q_EMIT changed(old[0], old[1], old[2]);
             }
